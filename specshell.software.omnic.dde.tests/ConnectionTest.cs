8d74103472345d988fa478a68c5a9d7bc67a3cb0
Jacob91 <Jacob.secher.andersen@gmail.com>
2020-11-20 15:54:29 +0100

    add new test

---
+++ specshell.software.omnic.dde.tests/ConnectionTest.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace specshell.software.omnic.dde.tests
+{
+    [TestClass]
+    public class ConnectionTest
+    {
+        [TestMethod]
+        [ExpectedException(typeof(NDde.DdeException))]
+        public void TestConnectionException()
+        {
+            Dde dde = new Dde();
+            dde.Connect();
+        }
+    }
+}
