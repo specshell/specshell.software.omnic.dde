7f8e07da1a6382e292d65f36e3b2087333d7e14d
Jacob91 <Jacob.secher.andersen@gmail.com>
2020-11-20 15:53:51 +0100

    add singleton implementation to dde class

--- specshell.software.omnic.dde/Dde.cs
+++ specshell.software.omnic.dde/Dde.cs
@@ -1,11 +1,23 @@
 ﻿using NDde.Client;
+using System;
 
 namespace specshell.software.omnic.dde
 {
-    class Dde
+    public class Dde
     {
-        private DdeClient client;
+        private static Dde _instance;
 
+        public static Dde Instance()
+        {
+            if (_instance == null)
+            {
+                _instance = new Dde();
+                _instance.Connect();
+            }
+            return _instance;
+        }
+
+        private DdeClient client;
         public Dde()
         {
             client = new DdeClient("OMNIC", "SPECTRA");
@@ -17,8 +29,21 @@ namespace specshell.software.omnic.dde
 
         public void Execute(string command, int timeout = 500)
         {
+            EnsureConnection();
             client.Execute(command, timeout);
         }
+
+        public string ExecuteWithResponse(string command, int timeout = 500)
+        {
+            Execute(command, timeout);
+            return ResultCurrent;
+        }
+
+        public void Poke(string item, string data, int timeout = 500)
+        {
+            client.Poke(item, data, timeout);
+        }
+
         public string Request(string item, int timeout = 500)
         {
             return client.Request(item, timeout);
@@ -26,9 +51,21 @@ namespace specshell.software.omnic.dde
 
         public string ResultCurrent => Request("Result Current", 1000);
 
-        public void Poke(string item, string data, int timeout = 500)
+        private void EnsureConnection()
         {
-            client.Poke(item, data, timeout);
+            if (!client.IsConnected)
+            {
+                Console.WriteLine("The client did NOT have a connection! Attempting to connect");
+                client.Connect();
+                if (client.IsConnected)
+                {
+                    Console.WriteLine("The client successfully established a new connection");
+                } 
+                else 
+                {
+                    Console.WriteLine("The client failed to establish a new connection!");
+                }
+            }
         }
     }
 }
