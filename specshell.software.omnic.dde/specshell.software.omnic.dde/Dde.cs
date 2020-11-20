5b093594abde44c74fd6e61cfcd858ec5041229c
Jacob91 <Jacob.secher.andersen@gmail.com>
2020-11-20 11:01:13 +0100

    add dde class from other module to this module

---
+++ specshell.software.omnic.dde/specshell.software.omnic.dde/Dde.cs
@@ -0,0 +1,34 @@
+﻿using NDde.Client;
+
+namespace specshell.software.omnic.dde
+{
+    class Dde
+    {
+        private DdeClient client;
+
+        public Dde()
+        {
+            client = new DdeClient("OMNIC", "SPECTRA");
+        }
+        public void Connect()
+        {
+            client.Connect();
+        }
+
+        public void Execute(string command, int timeout = 500)
+        {
+            client.Execute(command, timeout);
+        }
+        public string Request(string item, int timeout = 500)
+        {
+            return client.Request(item, timeout);
+        }
+
+        public string ResultCurrent => Request("Result Current", 1000);
+
+        public void Poke(string item, string data, int timeout = 500)
+        {
+            client.Poke(item, data, timeout);
+        }
+    }
+}
--- specshell.software.omnic.dde/specshell.software.omnic.dde/Program.cs
+++ specshell.software.omnic.dde/specshell.software.omnic.dde/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 
+using NDde.Client;
+
+
 namespace specshell.software.omnic.dde
 {
     class Program
@@ -7,6 +10,16 @@ namespace specshell.software.omnic.dde
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            string result = sendDDE("[About]");
+            Console.WriteLine("Result is " + result);
+        }
+
+        public static string sendDDE(string request)
+        {
+            Dde dde = new Dde();
+            dde.Connect();
+            dde.Execute(request, 1000);
+            return dde.ResultCurrent;
         }
     }
 }
