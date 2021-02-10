d4be4c82c35c178c27c3c7ad2c53356c5ec4fa10
Joseph Petersen <jpe@specshell.com>
2021-02-10 10:56:16 +0100

    add result array and error

---
+++ specshell.software.omnic.dde/Commands/Result.cs
@@ -0,0 +1,18 @@
+﻿namespace Specshell.Omnic.Dde.Commands
+{
+    public class Result : IDdeRequestCommand<string>
+    {
+        public Result(string parameter)
+        {
+            Command = $"Result {parameter}";
+        }
+
+        public static Result Current() => new Result("Current");
+
+        public static Result Array() => new Result("Array");
+
+        public static Result Error() => new Result("Error");
+
+        public string Command { get; }
+    }
+}
