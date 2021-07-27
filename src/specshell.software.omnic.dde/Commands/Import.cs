a1fb1bc8b267330d3178b8dee31aed6e8ba9f542
Joseph Petersen <jpe@specshell.com>
2021-07-27 18:28:55 +0200

    add import dde command (#7)

---
+++ src/specshell.software.omnic.dde/Commands/Import.cs
@@ -0,0 +1,12 @@
+﻿namespace Specshell.Omnic.Dde.Commands
+{
+    public class Import : IDdeExecuteCommand
+    {
+        public Import(string path = "")
+        {
+            Command = string.IsNullOrWhiteSpace(path) ? "[Import]" : $"[Import {path.DoubleQuote()}]";
+        }
+
+        public string Command { get; }
+    }
+}
