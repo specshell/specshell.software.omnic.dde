9f1874b564709cdff0eb15da9c71120f7b20980e
Joseph Petersen <jpe@specshell.com>
2021-08-10 15:00:57 +0200

    add SelectWindow,NewWindow,Paste,CutSelectedSpectra (#8)

---
+++ src/specshell.software.omnic.dde/Commands/Paste.cs
@@ -0,0 +1,12 @@
+﻿namespace Specshell.Omnic.Dde.Commands
+{
+    public class Paste :  IDdeExecuteCommand
+    {
+        public Paste()
+        {
+            Command = "[Paste]";
+        }
+
+        public string Command { get; }
+    }
+}
---
+++ src/specshell.software.omnic.dde/Commands/SelectWindow.cs
@@ -0,0 +1,12 @@
+﻿namespace Specshell.Omnic.Dde.Commands
+{
+    public class SelectWindow : IDdeExecuteCommand
+    {
+        public SelectWindow(string name)
+        {
+            Command = string.IsNullOrWhiteSpace(name) ? "[SelectWindow]" : $"[SelectWindow {name.DoubleQuote()}]";
+        }
+
+        public string Command { get; }
+    }
+}
